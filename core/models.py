481fe1b1dd195d83c37b07e03d6aa44cadbbb076
Rohit <rohitpratti@gmail.com>
2019-12-16 15:53:33 -0800

    updated with migrations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+
+    def _str_(self):
+        return self.title
+
+
+class OrderItem(models.Model):
+    pass
+
+    def _str_(self):
+        return self.title
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.title
+
